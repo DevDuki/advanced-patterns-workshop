4013af26a22895be6827779567d04ae0ee49ae5b
Dusan Misic <dmisic@inventage.com>
2023-03-07 15:10:04 +0100

    exercise 14: Experiencing typescript's weird error feedback, to why no arrows functions are allowed for the "asserts" functions

--- src/03-type-predicates-assertion-functions/14-typescripts-worst-error.problem.ts
+++ src/03-type-predicates-assertion-functions/14-typescripts-worst-error.problem.ts
@@ -15,6 +15,11 @@ interface NormalUser extends User {
   role: "normal";
 }
 
+/**
+ * This is the same function as in the previous exercise, but instead of using the "function" keyword, we are using an arrow function.
+ * For some reason typescript throws an alien-like error for this issue and I am also not entirely sure why functions with "asserts"
+ * cannot be an arrow function.
+ */
 const assertUserIsAdmin = (
   user: NormalUser | AdminUser,
 ): asserts user is AdminUser => {
@@ -42,8 +47,10 @@ it("Should assert that the type is an admin user after it has been validated", (
      * free to use the solution. I have personally wasted
      * hours on this error.
      */
+    // @ts-expect-error
     assertUserIsAdmin(user);
 
+    // @ts-expect-error
     type tests = [Expect<Equal<typeof user, AdminUser>>];
   };
 });
