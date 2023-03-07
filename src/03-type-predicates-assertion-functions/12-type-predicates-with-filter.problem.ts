241b3303ab6fca06e59a44d44335f1e4e54f2176
Dusan Misic <dmisic@inventage.com>
2023-03-07 11:27:04 +0100

    exercise 11: Use predicate in filter's callback function, to make sure that our returned type of the filter is correct and has no more undefined in its type.

--- src/03-type-predicates-assertion-functions/12-type-predicates-with-filter.problem.ts
+++ src/03-type-predicates-assertion-functions/12-type-predicates-with-filter.problem.ts
@@ -3,7 +3,21 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 export const values = ["a", "b", undefined, "c", undefined];
 
-const filteredValues = values.filter((value) => Boolean(value));
+/**
+ * Here we use a predicate to make sure that what this function returns is in fact a string and not string | undefined. Be aware tho,
+ * that type predicates are not 100% reliable. They are about as safe as the "as" keyword, because for example, in the function below,
+ * we could say that "value is undefined" and then after the filter function, we would have an array of undefined, which is not true.
+ * Nevertheless, they are pretty useful.
+ */
+const predicate = (value: string | undefined): value is string => {
+  return Boolean(value);
+};
+
+const filteredValues = values.filter(predicate);
+// or with arrow function
+const filteredValues2 = values.filter((value): value is string => {
+  return Boolean(value);
+});
 
 it("Should filter out the undefined values", () => {
   expect(filteredValues).toEqual(["a", "b", "c"]);
